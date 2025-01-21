0a7190685b6a630324135691472dcfdbaaa11b99
Duda <dudamoreira13047@gmail.com>
2025-01-21 16:37:48 -0400

    commitinho

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+
+
+
+
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulário de Cadastro</title>
+</head>
+<body>
+    <h1>Formulário de Cadastro<h1>
+
+    <form action="./resultado/dados.php?algo" method = "POST">
+        <input type="text" name = "nome">
+        <button type="submit">enviar</button>
+</body>
+</html>
